0752504137461c9a4fdbfff4da0f0bde6a095292
Manu Gutiérrez <manuel.gutierrez@abancagen.com>
2024-06-27 17:32:04 +0200

    [ANDROID] Set Splash in android App.

--- composeApp/src/androidMain/kotlin/com/sngular/marvelkmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/sngular/marvelkmp/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             EnableTransparentStatusBar()
